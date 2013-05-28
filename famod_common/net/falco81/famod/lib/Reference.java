0363c56e89a43ef2251d614b9b7ec32f5454d0ed
falco81 <janfaltynek@seznam.cz>
2013-05-28 23:25:52 +0200

    First test

---
+++ famod_common/net/falco81/famod/lib/Reference.java
@@ -0,0 +1,8 @@
+package net.falco81.famod.lib;
+
+public class Reference {
+
+    public static final String MOD_ID = "FaMod";
+    public static final String MOD_NAME = "FaMod";
+    public static final String MOD_VERSION = "0.0.1";
+}
