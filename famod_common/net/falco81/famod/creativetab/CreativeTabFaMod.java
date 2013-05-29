3bf188823d90bc9e824dcef7e2556b3bd00392e4
falco81 <janfaltynek@seznam.cz>
2013-05-29 21:07:21 +0200

    Oprava pojmenovani zalozky

--- famod_common/net/falco81/famod/creativetab/CreativeTabFaMod.java
+++ famod_common/net/falco81/famod/creativetab/CreativeTabFaMod.java
@@ -1,15 +1,18 @@
 package net.falco81.famod.creativetab;
 
+import net.falco81.famod.lib.Blockids;
+import net.falco81.famod.lib.Reference;
+import net.minecraft.creativetab.CreativeTabs;
+import cpw.mods.fml.common.registry.LanguageRegistry;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
-import net.falco81.famod.lib.Blockids;
-import net.minecraft.creativetab.CreativeTabs;
 
 public class CreativeTabFaMod extends CreativeTabs {
 
     public CreativeTabFaMod(int par1, String par2Str) {
 
         super(par1, par2Str);
+        LanguageRegistry.instance().addStringLocalization("itemGroup."+Reference.MOD_ID, Reference.MOD_ID);
     }
 
     @Override
@@ -21,5 +24,6 @@ public class CreativeTabFaMod extends CreativeTabs {
 
         return Blockids.FAMODSAND;
     }
+    
 
 }
